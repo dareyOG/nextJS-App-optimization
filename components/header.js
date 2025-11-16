cb382376f4e17b2a36c3522e519d3ea713b5550e
Oludire Damilare <oludire@gmail.com>
2025-11-16 16:16:57 +0100

    control image size

--- components/header.js
+++ components/header.js
@@ -7,7 +7,11 @@ export default function Header() {
   return (
     <header id='main-header'>
       <Link href='/'>
-        <Image src={logo} alt='Mobile phone with posts feed on it' />
+        <Image
+          src={logo}
+          sizes='10vw'
+          alt='Mobile phone with posts feed on it'
+        />
       </Link>
       <nav>
         <ul>
