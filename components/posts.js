be5dec8b52e5f729bc300c2acba7c3a58fc7c94d
Oludire Damilare <oludire@gmail.com>
2025-11-17 13:28:58 +0100

    configure css for images using fill prop

--- components/posts.js
+++ components/posts.js
@@ -9,10 +9,29 @@ import { togglePostLikeStatus } from '@/actions/posts';
 import { formatDate } from '@/lib/format';
 
 function Post({ post, action }) {
+  const imageLoader = (config) => {
+    // console.log(config);
+
+    // split image string path by  '/upload/'
+    const urlStart = config.src.split('/upload/')[0];
+    const urlEnd = config.src.split('/upload/')[1];
+
+    const transformations = `w_200,q_${config.quality}`;
+
+    return `${urlStart}/upload/${transformations}/${urlEnd}`;
+  };
+
   return (
     <article className='post'>
       <div className='post-image'>
-        <Image src={post.image} fill alt={post.title} />
+        <Image
+          src={post.image}
+          loader={imageLoader}
+          fill
+          sizes='33vw'
+          alt={post.title}
+          quality={50}
+        />
       </div>
       <div className='post-content'>
         <header>
